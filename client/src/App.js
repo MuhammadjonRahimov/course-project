eef3d84972d40928f40fa53157fa132089a3ae38
unknown <muhmmadjon141998@gmail.com>
2022-10-17 21:33:35 -0700

    I pulled the latest version of project

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,7 @@ import AppRouter from "./components/AppRouter";
 
 function App() {
 	return (
-<<<<<<< HEAD
-		<IsShownContext.Provider value={{ showSidebar, setShowSidebar, showAuthForm, showAuthFormHandler, hideAuthFormHandler }}>
-			<Header />
-			{showAuthForm && <RegisterOrLogin />}
-			{showSidebar && <Sidebar />}
-			<Section title='Last updates ' />
-			<Section title="Top 5 big collections" />
-		</IsShownContext.Provider>
-=======
 		<AppRouter />
->>>>>>> f1bf60f447faccaaab65c87f23560b30b37aae66
 	);
 }
 export default App;
