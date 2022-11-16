7ee4d4a4691f73eacab21a0ff905158aacf96ced
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 12:01:03 +0500

    added functionality to control the theme of the website

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,17 +1,18 @@
 import MyButton from './UI/button/MyButton';
 import Modal from './UI/Modal';
 
-import { useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import { AppContext } from '../context';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { locale } from '../data/locale';
+import useMode from '../hooks/use-mode';
 
 
 function Sidebar() {
 	const location = useLocation();
 	const navigate = useNavigate();
-	const { showSidebar, setShowSidebar, showAuthFormHandler, languageHandler, lang } =
-		useContext(AppContext);
+	const { showSidebar, setShowSidebar, showAuthFormHandler, languageHandler, lang, mode, setMode } = useContext(AppContext);
+
 	function closeSidebar() {
 		setShowSidebar(false);
 		document.body.style.overflow = "auto";
@@ -27,19 +28,25 @@ function Sidebar() {
 		navigate('/');
 		closeSidebar();
 	}
+	function handleMode() {
+		setMode(mode === "light" ? "dark" : "light");
+	}
 	const listContent = [{ text: 'Collections' }]
 	return (
 		<Modal>
 			<nav className={` bg-[#f4f7f6] w-80 h-[100vh] absolute z-[1] ${!showSidebar && 'translate-x-[-320px]'} ease-linear duration-[300ms]`}>
 				<ul>
 					<li>
-						<MyButton>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-							</svg>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-							</svg>
+						<MyButton onClick={handleMode}>
+							{mode === "light" ?
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+								</svg>
+								:
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+								</svg>
+							}
 						</MyButton>
 					</li>
 					<li className='p-5 border-b-[1px] border-[rgb(230, 234, 234)] font-bold text-2xl flex justify-between px-5'>
