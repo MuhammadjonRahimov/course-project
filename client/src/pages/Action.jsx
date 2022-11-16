1a489654c47fa48e494df210441e6d029335343e
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 18:42:43 +0500

    removed not useful code

--- client/src/pages/Action.jsx
+++ client/src/pages/Action.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import Table from "../components/table/Table";
 import Layout from "../components/UI/Layout";
 import { AppContext } from "../context";
 
 function Action() {
 	const { lang } = useContext(AppContext);
+
 	return (
 		<Layout>
 			<Table />
