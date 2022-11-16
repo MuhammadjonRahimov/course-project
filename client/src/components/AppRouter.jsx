e28ac5316af5a099bf9c11fde55d07c5047fec80
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 11:51:26 +0500

    added some code to control mode

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -5,12 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 import { publicRoutes, privateRoutes } from '../routes';
 import { locale } from '../data/locale';
 import useLocalStorage from '../hooks/use-localStorage';
+import useMode from '../hooks/use-mode';
 
 function AppRouter() {
 	const [showSidebar, setShowSidebar] = useState(false);
 	const [showAuthForm, setShowAuthForm] = useState(false);
 	const [isAuth, setIsAuth] = useState(false);
 	const { item: lang, setItem: setLang } = useLocalStorage("lang", "en");
+	const { mode, setMode } = useMode("mode", "light");
 
 	locale.setLanguage(lang);
 
@@ -37,6 +39,17 @@ function AppRouter() {
 		return () => document.removeEventListener('click', null);
 	}, []);
 
+	useEffect(function () {
+		const body = document.body;
+		if (mode === "dark") {
+			body.classList.add("dark");
+		} else {
+			body.classList.remove("dark");
+		}
+	}, [mode])
+
+
+
 	function languageHandler() {
 		setLang(lang === 'en' ? 'uz' : 'en');
 	}
@@ -61,6 +74,8 @@ function AppRouter() {
 				hideAuthFormHandler,
 				lang,
 				languageHandler,
+				mode,
+				setMode
 			}}
 		>
 			<Routes>
