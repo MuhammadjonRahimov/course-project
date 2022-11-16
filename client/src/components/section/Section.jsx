821f6072f823912c0dd28aa2a3773492601d4c7a
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 11:52:25 +0500

    removed not used code

--- client/src/components/section/Section.jsx
+++ client/src/components/section/Section.jsx
@@ -9,11 +9,6 @@ import { Link } from 'react-router-dom';
 function Section(props) {
 	return (
 		<section className="relative">
-			{/* <Link to="/collection/new" title="Add an item" className="absolute top-5 right-5">
-				<svg style={{ width: '50px', height: '50px' }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-				</svg>
-			</Link> */}
 			{props.title && <h1 className="text-center my-5 text-[25px] font-bold">{props.title}</h1>}
 			<div className={styles['section__row']}>
 				{props.dates.map((data, index) => <Card key={index} data={data} />)}
