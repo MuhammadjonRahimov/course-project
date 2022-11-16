43d7f1cc4c8b3b21d43889078de4ac2f32743f5d
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 12:00:19 +0500

    realised a functionality to handle the value of mode in localStorage

--- client/src/hooks/use-mode.js
+++ client/src/hooks/use-mode.js
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function useMode() {
+function useMode(key, value) {
+	const [mode, setMode] = useState(localStorage.getItem(key) || value);
 
+	useEffect(() => {
+		localStorage.setItem(key, mode);
+	}, [mode]);
+
+	return { mode, setMode }
 }
 
 export default useMode;
