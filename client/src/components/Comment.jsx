861ffcf0f3d6e64300d542250c5a2c69008372dd
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-16 12:09:53 +0500

    improved styles for both mode

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,18 +1,26 @@
+import { useContext } from 'react';
+import { AppContext } from '../context';
+
+
 function Comment({ comment }) {
+
+	const { mode } = useContext(AppContext);
+
+	const currentStyle = mode === "dark" ? "bg-[#f4f7f6] text-[rgb(20, 21, 21)] before:border-[#f4f7f6]" : "bg-[#444] text-white before:border-[#444]";
+
 	return (
 		<div className="mb-5">
-			<div className="flex items-center gap-2 mb-2">
-				<h3 className="text-[25px] font-bold text-red-900">{comment.name}</h3>
-				<p className="text-[16px] text-[rgb(20, 21, 21)]">
+			<div className="flex items-center gap-2 mb-4">
+				<h3 className="text-[25px] font-bold text-red-400">{comment.name}</h3>
+				<p className="text-[16px] text-red-400">
 					{comment.time.toLocaleDateString() + ' '}
-					{comment.time.toLocaleTimeString()}
 				</p>
 			</div>
 			<div>
-				<p className="bg-[#f4f7f6] text-[rgb(20, 21, 21)] p-2 rounded-[8px] relative
+				<p className={`p-2 rounded-[8px] relative
 				before:absolute before:left-[1%] before:top-0 before:w-0 before:h-0
 				before:border-b-[15px] before:border-b-[transparent] before:border-l-[25px]
-				before:border-[#f4f7f6] before:-translate-y-[100%] before:rotate-[-90deg]">
+				before:-translate-y-[100%] before:rotate-[-90deg] ${currentStyle}`}>
 					{comment.comment}
 				</p>
 			</div>
